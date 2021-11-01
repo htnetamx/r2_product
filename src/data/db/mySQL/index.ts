08f95c7ef286ab2cb0798e691c11d4e8afc8699d
david.gomez <david.gomez@teamcore.cl>
2021-11-01 10:44:22 -0300

    Version beta paginador

--- src/data/db/mySQL/index.ts
+++ src/data/db/mySQL/index.ts
@@ -42,4 +42,37 @@ export class RepositoryMySQL implements IProductRepository {
       return null;
     }
   }
+
+
+  async getAllProduct(numPerPage: any, page: any):  Promise<ProductBaseModel | null>{
+    try {
+      let numRows;
+      let queryPagination;
+      let numPages;
+      let skip = page * numPerPage;
+      let limit = skip + ',' + numPerPage;
+      if (Connection.mySQL2Pool == null) return null;
+      let [results, fields] = await Connection.mySQL2Pool.query({
+        sql: "SELECT count(*) as numRows from netamx.Product",
+      });
+      let data = Object.values(JSON.parse(JSON.stringify(results)));
+      numRows = data[0].numRows;
+      numPages = Math.ceil(numRows / numPerPage);
+      console.log('number of pages:', numPages);
+      let [results2, fields2] = await Connection.mySQL2Pool.query({
+        sql:  "SELECT * from netamx.Product order by ID desc limit=" + limit +";",
+      });
+      let data2 = Object.values(JSON.parse(JSON.stringify(results2)));
+      let result2 = data2.map<ProductBaseModel>((r) => {
+        return new ProductMapperMySQL().mapFrom(r as any);
+      });
+      result2
+
+      
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+
+  }
 }
--- src/server/routes/product/getAll.ts
+++ src/server/routes/product/getAll.ts
@@ -1,6 +1,5 @@
 import { Express, Request, Response, NextFunction } from "express";
 import { ProductController } from "../../../controller/product";
-
 export class getAllRoute {
   private server: Express;
 
@@ -10,6 +9,8 @@ export class getAllRoute {
 
   public getAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const numPerPage = parseInt(req.params.npp, 10) || 1;
+      const page = parseInt(req.params.page, 10) || 0;
       const result = await new ProductController().getAll();
       res.status(200).send(result);
     } catch (e) {}
